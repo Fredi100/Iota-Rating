488f963f6b902be01571b938fb1cfae827456fbb
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2021-01-08 19:02:39 +0100

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { BoothManagerComponent } from './booth-manager/booth-manager.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BoothManagerComponent } from './booth-manager/booth-manager.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
